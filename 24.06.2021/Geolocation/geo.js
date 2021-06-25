6ccf8e1a393db71a8e60aa11e1fd541e45b68546
sujatha1812 <sujathagopinath1899@gmail.com>
2021-06-25 19:46:36 +0530

    task

--- 24.06.2021/Geolocation/geo.js
+++ 24.06.2021/Geolocation/geo.js
@@ -1,14 +1,14 @@
-var c =  function() {
-        var lat = pos.coords.latitude,
-           long = pos.coords.longitude,
-           coords = lat + ', ' +long;
-           document.getElementById('google_map').setAttribute('src','https://maps.google.co.uk/?q=' + coords +',0&z=60&output=embed');
-
-        
-    }
+var c = function (position) {
+   var lat = position.coords.latitude;
+   var long = position.coords.longitude;
+   var coords = lat + ', ' + long;
+   document.getElementById('google_map').setAttribute('src', 'https://maps.google.co.uk/?q=' + coords + ',0&z=60&output=embed');
 
 
-    document.getElementById('get_location').onclick = function(){
-       navigator.geolocation.getCurrentPosition(c);
-       return false; 
-    }
+}
+
+
+document.getElementById('get_location').onclick = function () {
+   navigator.geolocation.getCurrentPosition(c);
+   return false;
+}
