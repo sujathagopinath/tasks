2bc4c3cbf88115a360b9a8af69625d2258170f17
sujatha1812 <sujathagopinath1899@gmail.com>
2021-06-29 21:37:45 +0530

    task

---
+++ 29.06.2021/template.js
@@ -0,0 +1,11 @@
+window.onload = function () {
+    // var temp = `hello, my name is madhu`;
+
+    // console.log(temp);
+
+    function details(name, age) {
+        console.log(`My name is ${name}  and age is ${age}`);
+        //and age is${10+12};
+    }
+    details("sujatha", 21);
+}
