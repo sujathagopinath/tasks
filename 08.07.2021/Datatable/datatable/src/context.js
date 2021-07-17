1b8856144a799332a3e2cb2d18e8967185ea058c
sujatha1812 <sujathagopinath1899@gmail.com>
2021-07-17 23:56:59 +0530

    17.07.2021

--- 08.07.2021/Datatable/datatable/src/context.js
+++ 08.07.2021/Datatable/datatable/src/context.js
@@ -17,17 +17,114 @@ class EmployeeProvider extends React.Component {  //provider
         updateEdit: []
     }
     getdata = (id) => {
-        const employee
+        const employee = this.state.Alldata.find(item => item.id === id);
+        return employee;
+
     }
+
     onEdit = (id) => {
         const tempemp = this.state.Alldata;
         const index = tempemp.indexOf(this.getdata(id));
+        const selected = tempemp[index];
+        this.setState({
+            id: selected['id'],
+            Name: selected['Name'],
+            Position: selected['Position'],
+            Office: selected['Office'],
+            Age: selected['Age'],
+            Startdate: selected['Startdate'],
+            Salary: selected['Salary']
+
+        })
+    }
+    updateValue = (e, test) => {
+        if (test === "Name") {
+            this.state.Name = e.target.value;
+        }
+        if (test === "Position") {
+            this.state.Position = e.target.value;
+        }
+        if (test === "Office") {
+            this.state.Office = e.target.value;
+        }
+        if (test === "Age") {
+            this.state.Age = e.target.value;
+        }
+        if (test === "Startdate") {
+            this.state.Startdate = e.target.value;
+        }
+        if (test === "Salary") {
+            this.state.Salary = e.target.value;
+        }
+        const temparr = [this.state.id, this.state.Name, this.state.Position, this.state.Office,
+        this.state.Age, this.state.Startdate, this.state.Salary];
+        this.setState({
+            updateEdit: temparr
+        })
+    }
+    onSave = (id) => {
+        if (id !== '') {
+            const Saved = this.state.Alldata;
+            const index = Saved.indexOf(this.getdata(id));
+            const Saveddata = Saved[index];
+            Saveddata['Name'] = this.state.updateEdit[1];
+            Saveddata['Position'] = this.state.updateEdit[2];
+            Saveddata['Office'] = this.state.updateEdit[3];
+            Saveddata['Age'] = this.state.updateEdit[4];
+            Saveddata['Startdate'] = this.state.updateEdit[5];
+            Saveddata['Salary'] = this.state.updateEdit[6];
+            this.setState({
+                Alldata: [...this.state.Alldata],
+                id: '',
+                Name: '',
+                Position: '',
+                Office: '',
+                Age: '',
+                Startdate: '',
+                Salary: ''
+            })
+
+        } else {
+            const MaxId = Math.max(...this.state.Alldata.map(item => item.id));
+            const id = MaxId + 1;
+            const newArr = [];
+            newArr['Name'] = this.state.updateEdit[1];
+            newArr['Position'] = this.state.updateEdit[2];
+            newArr['Office'] = this.state.updateEdit[3];
+            newArr['Age'] = this.state.updateEdit[4];
+            newArr['Startdate'] = this.state.updateEdit[5];
+            newArr['Salary'] = this.state.updateEdit[6];
+            this.setState({
+                Alldata: [...this.state.Alldata, newArr],
+                id: '',
+                Name: '',
+                Position: '',
+                Office: '',
+                Age: '',
+                Startdate: '',
+                Salary: ''
+            })
+
+        }
+
+    }
+    onDelete = (id) => {
+        const tempempo = this.state.Alldata.filter(item => item.id !== id);
+        this.setState({
+            Alldata: tempempo
+        })
     }
     render() {
         // console.log(this.state.Alldata);
         return (
             <div>
-                <EmployeeContext.Provider value={{ ...this.state }}>
+                <EmployeeContext.Provider value={{
+                    ...this.state,
+                    onEdit: this.onEdit,
+                    updateValue: this.updateValue,
+                    onSave: this.onSave,
+                    onDelete: this.onDelete
+                }}>
                     {this.props.children}
 
                 </EmployeeContext.Provider>
--- 08.07.2021/Datatable/datatable/src/home.js
+++ 08.07.2021/Datatable/datatable/src/home.js
@@ -26,12 +26,12 @@ class Home extends React.Component {
                                         </tr>
 
                                         <tr>
-                                            <td><input type="text" value={value.Name} onChange={(e) => { value.updatevalue(e, "Name") }} /></td>
-                                            <td><input type="text" value={value.Position} onChange={(e) => { value.updatevalue(e, "Position") }} /></td>
-                                            <td><input type="text" value={value.Office} onChange={(e) => { value.updatevalue(e, "Office") }} /></td>
-                                            <td><input type="text" value={value.Age} onChange={(e) => { value.updatevalue(e, "Age") }} /></td>
-                                            <td><input type="text" value={value.Startdate} onChange={(e) => { value.updatevalue(e, "Startdate") }} /></td>
-                                            <td><input type="text" value={value.Salary} onChange={(e) => { value.updatevalue(e, "Salary") }} /></td>
+                                            <td><input type="text" value={value.Name} onChange={(e) => { value.updateValue(e, "Name") }} /></td>
+                                            <td><input type="text" value={value.Position} onChange={(e) => { value.updateValue(e, "Position") }} /></td>
+                                            <td><input type="text" value={value.Office} onChange={(e) => { value.updateValue(e, "Office") }} /></td>
+                                            <td><input type="text" value={value.Age} onChange={(e) => { value.updateValue(e, "Age") }} /></td>
+                                            <td><input type="text" value={value.Startdate} onChange={(e) => { value.updateValue(e, "Startdate") }} /></td>
+                                            <td><input type="text" value={value.Salary} onChange={(e) => { value.updateValue(e, "Salary") }} /></td>
                                             <td><Button variant="primary" onClick={() => { value.onSave(value.id) }}>{value.id ? "Save" : "Add"}</Button></td>
                                         </tr>
 
@@ -45,7 +45,7 @@ class Home extends React.Component {
                                                     <td>{employee.Startdate}</td>
                                                     <td>{employee.Salary}</td>
                                                     <td><Button size="sm" variant="primary" onClick={() => { value.onEdit(employee.id) }}>Edit</Button><br />
-                                                        <Button size="sm" variant="danger" onClick={() => { value.onEdit(employee.id) }}>Delete</Button></td>
+                                                        <Button size="sm" variant="danger" onClick={() => { value.onDelete(employee.id) }}>Delete</Button></td>
                                                 </tr>
                                             )
                                         })}
