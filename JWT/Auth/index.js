8c5c35333859be3adb4184d35b5bbd9d1a7ce86f
sujatha1812 <sujathagopinath1899@gmail.com>
2021-07-31 22:26:11 +0530

    31.07.2021

--- JWT/Auth/index.js
+++ JWT/Auth/index.js
@@ -41,6 +41,36 @@ app.post('/login', (req, res) => {
 
 })
 
+app.post('/posts', verifyToken, (req, res) => {
+    jwt.verify(req.token, "some key", (err, authData) => {
+        if (err) {
+            res.sendStatus(403);
+        }
+        else {
+            res.json({
+                message: "successfully posted",
+                authData: authData
+            })
+        }
+    })
+
+})
+
+function verifyToken(req, res, next) {
+    const bearerHeader = req.headers['authorization'];
+    if (typeof bearerHeader !== 'undefined') {
+        const bearer = bearerHeader.split(' ');
+        const bearerToken = bearer[1];
+        req.token = bearerToken;
+        next();
+
+    }
+    else {
+        res.sendStatus(403);
+    }
+}
+
+
 app.listen(8008, (req, res) => {
     console.log("Serving at 8008!!");
 })
