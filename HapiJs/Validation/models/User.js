d4a5dfa01907fa137923bc8652fd6e82914e0c0e
sujatha1812 <sujathagopinath1899@gmail.com>
2021-09-18 22:32:43 +0530

    18.09.2021

--- HapiJs/Validation/models/User.js
+++ HapiJs/Validation/models/User.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 const userModel = mongoose.model("employees", {
-    firstname: String,
-    lastname: String,
+    username: String,
     password: String,
+    email: String,
     phonenumber: String
 });
+
+const salt = bcrypt.genSaltSync(10);
+const hashedPassword = bcrypt.hashSync(request.payload.password, salt);
 
 module.exports = userModel
--- HapiJs/Validation/server.js
+++ HapiJs/Validation/server.js
@@ -1,45 +1,51 @@
 const Hapi = require('@hapi/hapi');
 const Joi = require('@hapi/joi')
 const userModel = require('./models/User')
-const dbConnect = require('./config/dbConnect')
-dbConnect();
+const mongoose = require('mongoose')
+// const dbConnect = require('./config/dbConnect')
+// dbConnect();
+
+mongoose.connect(
+    "mongodb://localhost:27017/validation", {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("DB Connected")
+}).catch((error) => {
+    console.log(error)
+})
+
 
 const server = Hapi.server({
     host: 'localhost',
     port: 4000,
 })
 
-
 server.route({
     method: 'POST',
-    path: '/login',
+    path: '/signup',
     handler: async (request, h) => {
         try {
-            var user = new userModel(request.payload);
-            var result = await user.save()
-            return h.response(result)
+            const { username, email, password, phonenumber } = request.payload
+            const userExist = await userModel.findOne({ email: email })
+            if (userExist) {
+                return 'User has already taken'
+            }
+            const usercreated = await userModel.create({ username, password, email, phonenumber })
+            const result = await usercreated.save()
+            return h.response(result).code(200);
         } catch (error) {
             return h.response(error).code(500)
         }
     },
-
     options: {
         validate: {
             payload: Joi.object({
-                firstname: Joi.string().min(2).max(5).required(),
-                lastname: Joi.string().min(5).max(15).uppercase().required(),
+                username: Joi.string().min(2).max(10).required(),
+                email: Joi.string().min(6).required().email(),
                 password: Joi
                     .string()
-                    .required()
-                    .custom((value, helper) => {
-
-                        if (value.length < 8) {
-                            return helper.message("Password must be at least 8 characters long")
-
-                        } else {
-                            return true
-                        }
-                    }),
+                    .regex(/^[a-zA-Z0-9]{5,10}$/)
+                    .required(),
                 phonenumber: Joi
                     .string()
                     .regex(/^[7-9]\d{9}$/)
@@ -50,6 +56,14 @@ server.route({
                 return error.isJoi ? h.response(error.details[0]).takeover() : h.response(error).takeover();
             }
         }
+    }
+})
+
+server.route({
+    method: 'POST',
+    path: '/login',
+    handler: async (request, h) => {
+
     }
 })
 
@@ -67,17 +81,31 @@ server.route({
     }
 })
 
-// server.route({
-//     method: 'GET',
-//     path: '/user',
-//     handler: async (request, h) => {
-//         try {
+server.route({
+    method: 'PUT',
+    path: '/update/{id}',
+    handler: async (request, h) => {
+        try {
+            const person = await userModel.findByIdAndUpdate(request.params.id, request.payload)
+            return h.response(person)
+        } catch (error) {
+            return h.response(error).code(500)
+        }
+    },
+    options: {
+        validate: {
+            payload: Joi.object({
+                username: Joi.string().optional(),
+                phonenumber: Joi.string().regex(/^[7-9]\d{9}$/).required()
 
-//         } catch (error) {
+            }),
+            failAction: (request, h, error) => {
+                return error.isJoi ? h.response(error.details[0]).takeover() : h.response(error).takeover();
+            }
+        }
+    }
+})
 
-//         }
-//     }
-// })
 
 const init = async () => {
     await server.start();
